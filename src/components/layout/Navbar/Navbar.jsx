3e59af4dc7cd0b9a731504574dd12d781e9c03ca
nguyenbachduyhoang <nguyenduyhoang1494@gmail.com>
2025-06-02 19:10:26 +0700

    Fix structure

--- src/components/layout/Navbar/Navbar.jsx
+++ src/components/layout/Navbar/Navbar.jsx
@@ -4,12 +4,31 @@ import './Navbar.css';
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <ul>
+      <div className="navbar-logo">Physics Test System</div>
+      <ul className="navbar-menu">
         <li className="active">Trang chủ</li>
         <li>Tạo đề thi</li>
         <li>Ngân hàng câu hỏi</li>
         <li>Lịch sử</li>
       </ul>
+      <div className="navbar-actions">
+        <div className="navbar-search">
+          <input type="text" placeholder="Tìm kiếm..." />
+        </div>
+        <div className="navbar-avatar">
+          <img 
+            src="/path/to/avatar.png" 
+            alt="Avatar" 
+            className="avatar-image" 
+          />
+          <div className="avatar-dropdown">
+            <ul>
+              <li>Thông tin cá nhân</li>
+              <li>Đăng xuất</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </nav>
   );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from "react";
 import './Home.css';
+
 
 const Home = () => {
   return (
