d3a3ec066a4600c4a378c4b003b7387417a946d7
nguyenbachduyhoang <nguyenduyhoang1494@gmail.com>
2025-06-06 12:42:39 +0700

    fix: home

--- src/components/layout/Navbar/Navbar.jsx
+++ src/components/layout/Navbar/Navbar.jsx
@@ -1,25 +1,42 @@
 import React from 'react';
-import './Navbar.css';
+import { FaHome, FaFileAlt, FaRegCalendarAlt } from 'react-icons/fa';
+import { AiOutlineBank } from 'react-icons/ai';
+import './Navbar.scss';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="navbar-logo">Physics Test System</div>
+      <div className="navbar-logo">
+        <img src="/logo.png" alt="Logo" />
+        Physics Test System
+      </div>
       <ul className="navbar-menu">
-        <li className="active">Trang chủ</li>
-        <li>Tạo đề thi</li>
-        <li>Ngân hàng câu hỏi</li>
-        <li>Lịch sử</li>
+        <li className="active">
+          <FaHome />
+          Trang chủ
+        </li>
+        <li>
+          <FaFileAlt />
+          Tạo đề thi
+        </li>
+        <li>
+          <AiOutlineBank />
+          Ngân hàng câu hỏi
+        </li>
+        <li>
+          <FaRegCalendarAlt />
+          Lịch sử
+        </li>
       </ul>
       <div className="navbar-actions">
         <div className="navbar-search">
           <input type="text" placeholder="Tìm kiếm..." />
         </div>
         <div className="navbar-avatar">
-          <img 
-            src="/path/to/avatar.png" 
-            alt="Avatar" 
-            className="avatar-image" 
+          <img
+            src="/path/to/avatar.png"
+            alt="Avatar"
+            className="avatar-image"
           />
           <div className="avatar-dropdown">
             <ul>
